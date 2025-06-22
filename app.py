06432f1d7e8f30dfdebb1073af8db7e56be04e11
nero <misiektest745@gmail.com>
2025-06-22 19:59:05 +0200

    prep for rel v0.0.1

--- app.py
+++ app.py
@@ -184,5 +184,5 @@ def delete(filename):
     parent_path = os.path.dirname(filename)
     return redirect(url_for('browse', path=parent_path))
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+#if __name__ == '__main__':
+#    app.run(debug=True, host='0.0.0.0', port=5000)
